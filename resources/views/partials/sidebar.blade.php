9021cf273ba10a9a936129e5e9d3fd6670a30dc5
Abyan Falah <abyanf410@gmail.com>
2023-05-15 18:39:19 +0700

    mocking sidebar link untuk semua role

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -120,6 +120,216 @@
          @endif
 
          @if (auth()->user()->role->name == 'Admin')
+             {{-- DASHBOARD --}}
+             <li class="nav-item">
+                 <a class="nav-link {{ $active != 'dashboard' ? 'collapsed' : '' }} " href="/admin">
+                     <i class="bi bi-grid"></i>
+                     <span>Dashboard</span>
+                 </a>
+             </li>
+
+             {{-- PEMBUKUAN --}}
+             <li class="nav-item">
+                 <a class="nav-link {{ $active != 'buku' ? 'collapsed' : '' }}" data-bs-target="#menuPembukuan"
+                     data-bs-toggle="collapse" href="#">
+                     <i class="bi bi-menu-button-wide"></i><span>Pembukuan</span><i
+                         class="bi bi-chevron-down ms-auto"></i>
+                 </a>
+                 <ul id="menuPembukuan"
+                     class="nav-content  {{ $active != 'buku' ? 'collapse' : '' }} {{ $active == 'buku' ? 'show' : '' }}"
+                     data-bs-parent="#sidebar-nav">
+                     <li>
+                         <a href="/admin/kas" class="{{ $title == 'Buku - Kas' ? 'active' : '' }}">
+                             <i class="bi bi-circle"></i><span>Kas</span>
+                         </a>
+                     </li>
+                     <li>
+                         <a href="/admin/hutang" class="{{ $title == 'Buku - Hutang' ? 'active' : '' }}">
+                             <i class="bi bi-circle"></i><span>Hutang</span>
+                         </a>
+                     </li>
+                     <li>
+                         <a href="/admin/piutang" class="{{ $title == 'Buku - Piutang' ? 'active' : '' }}">
+                             <i class="bi bi-circle"></i><span>Piutang</span>
+                         </a>
+                     </li>
+                     <li>
+                         <a href="/admin/pakan" class="{{ $title == 'Buku - Pakan' ? 'active' : '' }}">
+                             <i class="bi bi-circle"></i><span>Pakan</span>
+                         </a>
+                     </li>
+                     <li>
+                         <a href="/admin/prive" class="{{ $title == 'Buku - Prive' ? 'active' : '' }}">
+                             <i class="bi bi-circle"></i><span>Prive</span>
+                         </a>
+                     </li>
+                     <li>
+                         <a href="/admin/gaji" class="">
+                             <i class="bi bi-circle"></i><span>Tabungan</span>
+                         </a>
+                     </li>
+                     <li>
+                         <a href="/admin/pekerja" class="{{ $title == 'Buku - Gaji' ? 'active' : '' }}">
+                             <i class="bi bi-circle"></i><span>Gaji</span>
+                         </a>
+                     </li>
+                     <li>
+                         <a href="/admin/servis_mobil">
+                             <i class="bi bi-circle"></i><span>Servis Mobil</span>
+                         </a>
+                     </li>
+                 </ul>
+             </li>
+
+             {{-- USER --}}
+             <li class="nav-item">
+                 <a class="nav-link {{ $active != 'users' ? 'collapsed' : '' }}" data-bs-target="#menuUser"
+                     data-bs-toggle="collapse" href="#">
+                     <i class="bi bi-people"></i><span>User</span><i class="bi bi-chevron-down ms-auto"></i>
+                 </a>
+                 <ul id="menuUser"
+                     class="nav-content   {{ $active != 'users' ? 'collapse' : '' }} {{ $active == 'users' ? 'show' : '' }}"
+                     data-bs-parent="#sidebar-nav">
+                     <li>
+                         <a href="/admin/customer" class="{{ $title == 'Customer' ? 'active' : '' }}">
+                             <i class="bi bi-circle"></i><span>Customer</span>
+                         </a>
+                     </li>
+                     <li>
+                         <a href="/admin/supplier_sapi" class="{{ $title == 'Supplier Sapi' ? 'active' : '' }}">
+                             <i class="bi bi-circle"></i><span>Supplier Sapi</span>
+                         </a>
+                     </li>
+                     <li>
+                         <a href="/admin/supplier_pakan">
+                             <i class="bi bi-circle"></i><span>Supplier Pakan</span>
+                         </a>
+                     </li>
+                     <li>
+                         <a href="/admin/akuntan">
+                             <i class="bi bi-circle"></i><span>Akuntan</span>
+                         </a>
+                     </li>
+                     <li>
+                         <a href="/admin/admin">
+                             <i class="bi bi-circle"></i><span>Admin</span>
+                         </a>
+                     </li>
+                     <li>
+                         <a href="/admin/owner" class="">
+                             <i class="bi bi-circle"></i><span>Owner</span>
+                         </a>
+                     </li>
+                 </ul>
+             </li>
+         @endif
+
+         @if (auth()->user()->role->name == 'Owner')
+             {{-- DASHBOARD --}}
+             <li class="nav-item">
+                 <a class="nav-link {{ $active != 'dashboard' ? 'collapsed' : '' }} " href="/owner">
+                     <i class="bi bi-grid"></i>
+                     <span>Dashboard</span>
+                 </a>
+             </li>
+
+             {{-- PEMBUKUAN --}}
+             <li class="nav-item">
+                 <a class="nav-link {{ $active != 'buku' ? 'collapsed' : '' }}" data-bs-target="#menuPembukuan"
+                     data-bs-toggle="collapse" href="#">
+                     <i class="bi bi-menu-button-wide"></i><span>Pembukuan</span><i
+                         class="bi bi-chevron-down ms-auto"></i>
+                 </a>
+                 <ul id="menuPembukuan"
+                     class="nav-content  {{ $active != 'buku' ? 'collapse' : '' }} {{ $active == 'buku' ? 'show' : '' }}"
+                     data-bs-parent="#sidebar-nav">
+                     <li>
+                         <a href="/owner/kas" class="{{ $title == 'Buku - Kas' ? 'active' : '' }}">
+                             <i class="bi bi-circle"></i><span>Kas</span>
+                         </a>
+                     </li>
+                     <li>
+                         <a href="/owner/hutang" class="{{ $title == 'Buku - Hutang' ? 'active' : '' }}">
+                             <i class="bi bi-circle"></i><span>Hutang</span>
+                         </a>
+                     </li>
+                     <li>
+                         <a href="/owner/piutang" class="{{ $title == 'Buku - Piutang' ? 'active' : '' }}">
+                             <i class="bi bi-circle"></i><span>Piutang</span>
+                         </a>
+                     </li>
+                     <li>
+                         <a href="/owner/pakan" class="{{ $title == 'Buku - Pakan' ? 'active' : '' }}">
+                             <i class="bi bi-circle"></i><span>Pakan</span>
+                         </a>
+                     </li>
+                     <li>
+                         <a href="/owner/prive" class="{{ $title == 'Buku - Prive' ? 'active' : '' }}">
+                             <i class="bi bi-circle"></i><span>Prive</span>
+                         </a>
+                     </li>
+                     <li>
+                         <a href="/owner/gaji" class="">
+                             <i class="bi bi-circle"></i><span>Tabungan</span>
+                         </a>
+                     </li>
+                     <li>
+                         <a href="/owner/pekerja" class="{{ $title == 'Buku - Gaji' ? 'active' : '' }}">
+                             <i class="bi bi-circle"></i><span>Gaji</span>
+                         </a>
+                     </li>
+                     <li>
+                         <a href="/owner/servis_mobil">
+                             <i class="bi bi-circle"></i><span>Servis Mobil</span>
+                         </a>
+                     </li>
+                 </ul>
+             </li>
+
+             {{-- USER --}}
+             <li class="nav-item">
+                 <a class="nav-link {{ $active != 'users' ? 'collapsed' : '' }}" data-bs-target="#menuUser"
+                     data-bs-toggle="collapse" href="#">
+                     <i class="bi bi-people"></i><span>User</span><i class="bi bi-chevron-down ms-auto"></i>
+                 </a>
+                 <ul id="menuUser"
+                     class="nav-content   {{ $active != 'users' ? 'collapse' : '' }} {{ $active == 'users' ? 'show' : '' }}"
+                     data-bs-parent="#sidebar-nav">
+                     <li>
+                         <a href="/owner/customer" class="{{ $title == 'Customer' ? 'active' : '' }}">
+                             <i class="bi bi-circle"></i><span>Customer</span>
+                         </a>
+                     </li>
+                     <li>
+                         <a href="/owner/supplier_sapi" class="{{ $title == 'Supplier Sapi' ? 'active' : '' }}">
+                             <i class="bi bi-circle"></i><span>Supplier Sapi</span>
+                         </a>
+                     </li>
+                     <li>
+                         <a href="/owner/supplier_pakan">
+                             <i class="bi bi-circle"></i><span>Supplier Pakan</span>
+                         </a>
+                     </li>
+                     <li>
+                         <a href="/owner/akuntan">
+                             <i class="bi bi-circle"></i><span>Akuntan</span>
+                         </a>
+                     </li>
+                     <li>
+                         <a href="/owner/admin">
+                             <i class="bi bi-circle"></i><span>Admin</span>
+                         </a>
+                     </li>
+                     <li>
+                         <a href="/owner/owner" class="">
+                             <i class="bi bi-circle"></i><span>Owner</span>
+                         </a>
+                     </li>
+                 </ul>
+             </li>
+         @endif
+
+         {{-- @if (auth()->user()->role->name == 'Admin')
              <li class="nav-item">
                  <a class="nav-link {{ $active != 'dashboard' ? 'collapsed' : '' }} " href="/admin">
                      <i class="bi bi-grid"></i>
@@ -143,7 +353,7 @@
                      <span>Dashboard</span>
                  </a>
              </li><!-- End Dashboard Nav -->
-         @endif
+         @endif --}}
      </ul>
 
  </aside><!-- End Sidebar-->
