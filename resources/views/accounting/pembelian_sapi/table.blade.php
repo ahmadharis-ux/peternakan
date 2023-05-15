2ce2262f5d972b03205bbba410d55723fb1190ee
Abyan Falah <abyanf410@gmail.com>
2023-05-15 23:52:36 +0700

    table pembelian sapi

--- resources/views/accounting/pembelian_sapi/table.blade.php
+++ resources/views/accounting/pembelian_sapi/table.blade.php
@@ -8,10 +8,26 @@
             <th scope="col">Kredit</th>
             {{-- <th scope="col">Saldo</th> --}}
             <th scope="col">Jurnal</th>
-            <th scope="col">Detail</th>
+            {{-- <th scope="col">Detail</th> --}}
         </tr>
     </thead>
     <tbody>
+        @foreach ($listPembelianSapi as $pembelianSapi)
+            <tr>
+                <th scope="row">{{ $loop->iteration }}</th>
+                <td>{{ $pembelianSapi->created_at }}</td>
+                <td>{{ $pembelianSapi->kredit->pihakKedua->nama_depan }}</td>
+                <td>{{ $pembelianSapi->keterangan }}</td>
+                <td>[sum_kredit_transaksi_kreditSapi_nominal]</td>
+                <td>
+                    <a href="/acc/hutang/{{ $pembelianSapi->kredit->id }}" class="btn btn-primary">
+                        <div class="icon">
+                            <i class="bi bi-eye-fill"></i>
+                        </div>
+                    </a>
+                </td>
 
+            </tr>
+        @endforeach
     </tbody>
 </table>
