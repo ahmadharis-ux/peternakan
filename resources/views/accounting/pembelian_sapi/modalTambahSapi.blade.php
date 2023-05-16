3d2888d2b91d8042c3491c706bd01a531fc91a98
Abyan Falah <abyanf410@gmail.com>
2023-05-16 18:22:28 +0700

    Update modalTambahSapi.blade.php: ngereyeh opsi beli sapi (kiloan/perekor)

--- resources/views/accounting/pembelian_sapi/modalTambahSapi.blade.php
+++ resources/views/accounting/pembelian_sapi/modalTambahSapi.blade.php
@@ -5,13 +5,15 @@
 
             <form action="/storesapi" method="post" enctype="multipart/form-data">
                 @csrf
+                {{-- <input type="hidden" name="kas_id" value="" class="form-control"> --}}
+
 
                 <div class="modal-header">
                     <h1 class="modal-title fs-5" id="staticBackdropLabel">Tambah sapi</h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
 
-                <div class="modal-body">
+                <div class="modal-body p-5">
 
                     {{-- jenis sapi --}}
                     <div class="row mb-3">
@@ -25,20 +27,48 @@
                         </div>
                     </div>
 
-                    <div class="row">
-                        {{-- <input type="hidden" name="kas_id" value="" class="form-control"> --}}
+                    {{-- jenis kelamin sapi  --}}
+                    <div class="row mb-3">
+                        <div class="col">
+                            <label for="">Jenis kelamin sapi</label>
+                            <select name="jenis_sapi" class="form-select">
+                                <option value="jantan" selected>Pejantan</option>
+                                <option value="betina">Betina</option>
+                            </select>
+                        </div>
+                    </div>
 
-                        <div class="col-sm-4 mb-3">
+                    {{-- eartag dan bobot --}}
+                    <div class="row mb-3">
+                        <div class="col">
                             <label for="">Eartag</label>
                             <input type="text" name="eartag" value="" class="form-control" required>
                         </div>
 
-                        <div class="col-sm-4 mb-3">
+                        <div class="col">
                             <label for="">Bobot (kg)</label>
                             <input type="number" name="bobot" value="" class="form-control number-only"
                                 required>
                         </div>
                     </div>
+
+
+                    {{-- opsi pembelian --}}
+                    <div class="row mb-3 form-check">
+                        <p>Opsi pembelian</p>
+                        <div class="col">
+                            <label for="">Kiloan</label>
+                            <input type="radio" name="opsi_beli" value="kiloan" class="form-check-input">
+                        </div>
+
+                        <div class="col">
+                            <label for="">Per ekor</label>
+                            <input type="radio" name="opsi_beli" value="ekoran" class="form-check-input">
+                        </div>
+                    </div>
+
+
+
 
 
                     <div class="col-sm-4 mb-3">
