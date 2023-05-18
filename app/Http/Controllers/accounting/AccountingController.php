d3fceb1ea8b86a45927240db4b2aeabdccf9997b
Abyan Falah <abyanf410@gmail.com>
2023-05-18 15:36:45 +0700

    Update AccountingController.php

--- app/Http/Controllers/accounting/AccountingController.php
+++ app/Http/Controllers/accounting/AccountingController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers\accounting;
 
+use App\Models\Debit;
+use App\Models\Kredit;
 use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
 use App\Http\Controllers\Controller;
+use App\Models\Sapi;
 
 class AccountingController extends Controller
 {
@@ -15,6 +18,9 @@ class AccountingController extends Controller
             'heading' => 'Accounting',
             'active' => 'dashboard',
             'date' => Carbon::now()->format('d-m-Y'),
+            'totalHutang' => Kredit::all()->sum('nominal'),
+            'totalPiutang' => Debit::all()->sum('nominal'),
+            'stokSapi' => Sapi::where('status', 'ADA')->get()->count(),
         ];
 
         return view('accounting.index', $pageData);
