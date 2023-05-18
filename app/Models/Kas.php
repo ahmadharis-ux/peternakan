4101ecfb80a75f9367fff47787cbc6a13100eaa7
Abyan Falah <abyanf410@gmail.com>
2023-05-18 16:34:11 +0700

    Update Kas.php

---
+++ app/Models/Kas.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Kas extends Model
+{
+    use HasFactory;
+
+    public static function getAll()
+    {
+        $kolomDipilih = [
+            'id',
+            'id_jurnal',
+            'id_author',
+            'id_pihak_kedua',
+            'nominal',
+            'keterangan',
+            'lunas',
+            'created_at',
+        ];
+
+        $kredit = DB::table('kredits')->select($kolomDipilih);
+
+        $debit = DB::table('debits')->select($kolomDipilih);
+
+        return $kredit->union($debit)->orderBy('created_at')->get();
+    }
+
+    public static function getByIdJurnal($idJurnal)
+    {
+        $kolomDipilih = [
+            'id',
+            'id_jurnal',
+            'id_author',
+            'id_pihak_kedua',
+            'nominal',
+            'keterangan',
+            'lunas',
+            'created_at',
+        ];
+
+        $kredit = DB::table('kredits')->select($kolomDipilih)->where('id_jurnal', $idJurnal);
+
+        $debit = DB::table('debits')->select($kolomDipilih)->where('id_jurnal', $idJurnal);
+
+
+
+        return $kredit->union($debit)->orderBy('created_at')->get();
+    }
+}
