72a6324d4d8cdc210ca98fd23b0ddf8bb3e5147c
Abyan Falah <abyanf410@gmail.com>
2023-05-20 15:53:20 +0700

    Update 2023_05_20_085130_create_operasional_penjualan_sapi_table.php: POHO ANJIRRRRR

---
+++ database/migrations/2023_05_20_085130_create_operasional_penjualan_sapi_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('operasional_penjualan_sapi', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_penjualan_sapi')->unsigned();
+            $table->integer('harga');
+            $table->string('keterangan');
+
+            $table->timestamps();
+            $table->foreign('id_penjualan_sapi')->references('id')->on('penjualan_sapis');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('operasional_penjualan_sapi');
+    }
+};
