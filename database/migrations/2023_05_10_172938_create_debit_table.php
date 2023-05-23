1ee5e96687689b062f6ebc36239906aedcd16e4a
Abyan Falah <abyanf410@gmail.com>
2023-05-23 14:58:25 +0700

    tambah field id_kas di kredit dan debit

--- database/migrations/2023_05_10_172938_create_debit_table.php
+++ database/migrations/2023_05_10_172938_create_debit_table.php
@@ -13,15 +13,19 @@ return new class extends Migration
     {
         Schema::create('debits', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('id_kas')->unsigned();
             $table->foreignId('id_jurnal')->unsigned();
             $table->foreignId('id_author')->unsigned();
             $table->foreignId('id_pihak_kedua')->unsigned();
             $table->unsignedInteger('nominal')->default(0);
             $table->string('keterangan')->nullable();
             $table->boolean('lunas')->default(false);
+            $table->date('tenggat')->nullable();
+
 
             $table->timestamps();
 
+            $table->foreign('id_kas')->references('id')->on('kas');
             $table->foreign('id_jurnal')->references('id')->on('jurnals');
             $table->foreign('id_author')->references('id')->on('users');
             $table->foreign('id_pihak_kedua')->references('id')->on('users');
