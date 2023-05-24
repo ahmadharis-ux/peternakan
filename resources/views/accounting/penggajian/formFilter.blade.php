1b5e3ee17517f89b7814b7e8d53bc2069119bc4a
Abyan Falah <abyanf410@gmail.com>
2023-05-24 20:11:04 +0700

    Update PenggajianController.php, detail.blade.php, and 12 more files...

---
+++ resources/views/accounting/penggajian/formFilter.blade.php
@@ -0,0 +1,84 @@
+@php
+    $listBulan = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'];
+
+    $unixTime = strtotime(carbonToday());
+    $numBulanSekarang = getDate($unixTime)['mon'];
+@endphp
+
+
+<div class="container mb-3">
+    <form action="/acc/gaji/pekerja/{{ $pekerja->id }}" method="get">
+
+
+        <div class="d-flex flex-row justify-content-start flex-wrap mb-3">
+
+
+            {{-- tahun --}}
+            {{-- <div class="mb-3 me-3 col-3">
+                <label class="form-label">Tahun</label>
+                <select name="tahun" class="form-select">
+                    @foreach ($listTahun as $tahun)
+                        @php
+                            $numTahun = $loop->iteration;
+                        @endphp
+
+                        <option value="{{ $numTahun }}" {{ $numTahun == $numTahunSekarang ? 'selected' : '' }}>
+                            {{ $tahun }}</option>
+                    @endforeach
+                </select>
+            </div> --}}
+
+
+            {{-- bulan --}}
+            <div class="mb-3 me-3 col-3">
+                <label class="form-label">Bulan</label>
+                <select name="bulan" class="form-select">
+                    @foreach ($listBulan as $bulan)
+                        @php
+                            $numBulan = $loop->iteration;
+                        @endphp
+
+                        <option value="{{ $numBulan }}" {{ $numBulan == $numBulanSekarang ? 'selected' : '' }}>
+                            {{ $bulan }}</option>
+                    @endforeach
+                </select>
+            </div>
+
+
+            {{-- Tahun --}}
+            <div class="mb-3 me-3 col-3">
+                <label class="form-label">Tahun</label>
+                <select name="bulan" class="form-select">
+                    @for ($i = 2023; $i <= 2030; $i++)
+                        <option value="{{ $i }}">
+                            {{ $i }}</option>
+                    @endfor
+                </select>
+            </div>
+
+
+
+
+        </div>
+
+        <div class="d-flex justify-content-start">
+            <input id="btnSubmitFilter" class="btn btn-primary me-3" type="submit" value="Terapkan filter" hidden>
+
+            {{--
+            @if ($filtered)
+                <a href="/acc/sapi" class="btn btn-secondary me-3">Hapus filter</a>
+            @endif --}}
+
+        </div>
+
+    </form>
+</div>
+
+
+<script>
+    $(document).ready(function() {
+        $("select").change(function() {
+            $("#btnSubmitFilter").removeAttr('hidden')
+        })
+    })
+</script>
---
+++ resources/views/accounting/penggajian/modalCreate.blade.php
@@ -0,0 +1,35 @@
+  <div class="modal fade" id="modalCreatePembelianSapi" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+      aria-labelledby="staticBackdropLabel" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered">
+          <div class="modal-content">
+              <form action="/acc/hutang/" method="post" enctype="multipart/form-data">
+                  @csrf
+                  <div class="modal-header">
+                      <h1 class="modal-title fs-5" id="staticBackdropLabel">Kas Hutang</h1>
+                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                  </div>
+
+                  <div class="modal-body p-5">
+                      <div class="col-sm-12">
+                          <label for="">Supplier Sapi</label>
+                          <select name="id_pihak_kedua" id="" class="form-select">
+                              @foreach ($listSupplierSapi as $supplier)
+                                  <option value="{{ $supplier->id }}">{{ $supplier->fullname }}
+                                  </option>
+                              @endforeach
+                          </select>
+                      </div>
+                      <div class="col-sm-12">
+                          <label for="">Keterangan</label>
+                          <textarea required name="keterangan" class="form-control" placeholder="keterangan" id="" cols="30"
+                              rows="10">asdfadf</textarea>
+                      </div>
+                  </div>
+                  <div class="modal-footer">
+                      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                      <button type="submit" class="btn btn-primary">Simpan</button>
+                  </div>
+              </form>
+          </div>
+      </div>
+  </div>
