8ee71ac985097de86a68ea635686b91151e8086a
ahmadharis-ux <ahmadfreedom27@gmail.com>
2023-06-25 20:49:35 +0700

    Update ProfileController.php and 1687700081105-054309700_1610007641-pexels-rahul-695644.jpg

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Storage;
+
+class ProfileController extends Controller
+{
+    function index(){
+        $title = 'Profile';
+        $heading = 'Admin';
+        $active = 'profle';
+        return view('profile', [
+            'title' => $title,
+            'heading' => $heading,
+            'active' => $active,
+        ]);
+    }
+    function edit(){
+        $title = 'Profile';
+        $heading = 'Admin';
+        $active = 'profle';
+        return view('edit_profile', [
+            'title' => $title,
+            'heading' => $heading,
+            'active' => $active,
+        ]);
+    }
+    function update(Request $request)
+    {
+        $id = Auth::id();
+        $validasiData = $request->validate([
+            'nama_depan' => 'required',
+            'nama_belakang' => 'required',
+            'alamat' => 'nullable',
+            'foto_ttd' => 'nullable',
+            'telepon' => 'nullable',
+        ]);
+
+        if ($request->hasFile('foto_ttd')) {
+            // Hapus foto sebelumnya
+            $user = User::find($id);
+            if ($user->foto_ttd) {
+                Storage::delete($user->foto_ttd);
+            }
+
+            // Simpan foto baru
+            $path = $request->file('foto_ttd')->store('public/foto_ttd');
+            $validasiData['foto_ttd'] = $path;
+        }
+
+        User::where('id', $id)->update($validasiData);
+        return redirect('/profile');
+    }
+}
