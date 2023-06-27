e67538e8e54a1bb7fbbd4d2edcb1c0a2ba0ff5f7
abyanfalah <abyanf410@gmail.com>
2023-06-27 08:05:26 +0700

    Update edit_profile.blade.php and profile.blade.php

--- resources/views/edit_profile.blade.php
+++ resources/views/edit_profile.blade.php
@@ -1,112 +1,138 @@
 @extends('layouts.main')
 @section('container')
-<section class="section profile">
-  <div class="row" style="align-content: center; padding-left:15em;padding-right:15em">
-    <div class="col">
+    <section class="section">
+        <div class="row">
+            <div class="col col-lg-8 mx-auto">
 
-      <div class="card">
-        <div class="card-header">
-            Form Edit Profile
-        </div>
-        <form action="/updateprofile" method="POST" enctype="multipart/form-data">
-            @csrf
-            @method('PUT')
-            <div class="card-body">
-                <div class="row">
-                    <div class="col">
-                        <label for="" style="font-size: 12px">Nama Depan</label>
-                        <input name="nama_depan" type="text" class="form-control form-control-sm" value="{{auth()->user()->nama_depan}}">
+                <div class="card">
+                    <div class="card-header">
+                        Form Edit Profile
                     </div>
-                    <div class="col">
-                        <label for="" style="font-size: 12px">Nama Belakang</label>
-                        <input name="nama_belakang" type="text" class="form-control form-control-sm" value="{{auth()->user()->nama_belakang}}">
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col">
-                        <label for="" style="font-size: 12px">Perusahaan</label>
-                        <input type="text" class="form-control form-control-sm" value="Diva's Cow" disabled>
-                    </div>
-                    <div class="col">
-                        <label for="" style="font-size: 12px">Job</label>
-                        <input type="text" class="form-control form-control-sm" value="{{auth()->user()->role->nama}}" disabled>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col">
-                        <label for="" style="font-size: 12px">Telepon</label>
-                        <input name="telepon" type="text" class="form-control form-control-sm" value="{{auth()->user()->telepon}}" >
-                    </div>
-                    <div class="col">
-                        <label for="" style="font-size: 12px">Email</label>
-                        <input name="email" type="text" class="form-control form-control-sm" value="{{auth()->user()->email}}">
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col" style="width: max-content">
-                        <label for="" style="font-size: 12px">Alamat</label>
-                        <input name="alamat" type="text" class="form-control form-control-sm" value="{{auth()->user()->alamat}}">
-                    </div>
-                </div>
-                <div class="row text-center">
-                    <div class="col" style="width: max-content">
-                        <label for="">Tanda Tangan</label>
-                        <div class="gambar-ttd">
-                            <img src="{{ Storage::url(auth()->user()->foto_ttd) }}" id="previewImage" src="" alt="Preview Image" >
+                    <form action="/updateprofile" method="POST" enctype="multipart/form-data">
+                        @csrf
+                        @method('PUT')
+                        <div class="card-body">
+
+                            <div class="row mb-2">
+                                <div class="col">
+                                    <label style="font-size: 12px">Nama Depan</label>
+                                    <input name="nama_depan" type="text" class="form-control form-control-sm"
+                                        value="{{ auth()->user()->nama_depan }}">
+                                </div>
+                                <div class="col">
+                                    <label style="font-size: 12px">Nama Belakang</label>
+                                    <input name="nama_belakang" type="text" class="form-control form-control-sm"
+                                        value="{{ auth()->user()->nama_belakang }}">
+                                </div>
+                            </div>
+
+                            <div class="row mb-2">
+                                <div class="col">
+                                    <label style="font-size: 12px">Perusahaan</label>
+                                    <input type="text" class="form-control form-control-sm" value="Diva's Cow" disabled>
+                                </div>
+                                <div class="col">
+                                    <label style="font-size: 12px">Job</label>
+                                    <input type="text" class="form-control form-control-sm"
+                                        value="{{ auth()->user()->role->nama }}" disabled>
+                                </div>
+                            </div>
+
+                            <div class="row mb-2">
+                                <div class="col">
+                                    <label style="font-size: 12px">Telepon</label>
+                                    <input name="telepon" type="text" class="form-control form-control-sm"
+                                        value="{{ auth()->user()->telepon }}">
+                                </div>
+                                <div class="col">
+                                    <label style="font-size: 12px">Email</label>
+                                    <input name="email" type="text" class="form-control form-control-sm"
+                                        value="{{ auth()->user()->email }}">
+                                </div>
+                            </div>
+
+                            <div class="row mb-2">
+                                <div class="col" style="width: max-content">
+                                    <label style="font-size: 12px">Alamat</label>
+                                    <input name="alamat" type="text" class="form-control form-control-sm"
+                                        value="{{ auth()->user()->alamat }}">
+                                </div>
+                            </div>
+
+                            {{-- foto --}}
+                            <div class="row my-3">
+                                <div class="col d-flex flex-column align-items-center" style="width: max-content">
+                                    <label>Foto profil</label>
+                                    <div class="gambar-ttd my-2">
+                                        <img src="{{ Storage::url(auth()->user()->foto_ttd) }}" id="previewImage">
+                                    </div>
+                                    <input name="foto_ttd" type="file" id="myFile" class="button-file" />
+                                </div>
+
+                                <div class="col d-flex flex-column align-items-center" style="width: max-content">
+                                    <label>Tanda Tangan</label>
+                                    <div class="gambar-ttd my-2">
+                                        <img src="{{ Storage::url(auth()->user()->foto_ttd) }}" id="previewImage">
+                                    </div>
+                                    <input name="foto_ttd" type="file" id="myFile" class="button-file" />
+                                </div>
+
+
+                            </div>
+                            <div class="d-block mt-5 text-end">
+                                <button type="submit" class="btn btn-primary">Simpan</button>
+                            </div>
                         </div>
-                    </div>
+                    </form>
                 </div>
-                <input name="foto_ttd" type="file" id="myFile" class="button-file"  />
-                <div class="row">
-                    <button type="submit" class="button-simpan">Simpan</button>
-                </div>
+
             </div>
-        </form>
-      </div>
 
-    </div>
+        </div>
+    </section>
+    <style>
+        * {
+            /* border: 1px red solid; */
+        }
 
-  </div>
-</section>
+        .text-center {
+            display: flex;
+            justify-content: center;
+        }
 
-<style>
-    .text-center{
-        display: flex;
-        justify-content: center;
-    }
-    .gambar-ttd {
-        width: 200px;
-        height: 200px;
-        border-radius: 50%;
-        overflow: hidden;
-        justify-content: center;
-        margin-left: 200px;
-        border: 1px solid rgb(6, 41, 75);
-    }
+        .gambar-ttd {
+            width: 150px;
+            height: 150px;
+            border-radius: 50%;
+            overflow: hidden;
+            justify-content: center;
+            /* margin-left: 200px; */
+            border: 1px solid rgb(6, 41, 75);
+        }
 
-    .gambar-ttd img {
-        display: block;
-        width: 100%;
-        height: 100%;
-        object-fit: cover;
-    }
-    .button-simpan{
-        background: rgb(8, 149, 224);
-        color: white;
-        border-radius: 5px;
-        margin-top: 5px;
-        width:100px;
-        border: 0px;
-        margin-left: 500px
-    }
-    .button-file{
-        width: 100px;
-        margin-top:5px;
-        margin-left: 15.5em;
-    }
-   
-    
-</style>
+        .gambar-ttd img {
+            display: block;
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+        }
+
+        .button-simpan {
+            background: rgb(8, 149, 224);
+            color: white;
+            border-radius: 5px;
+            margin-top: 5px;
+            width: 100px;
+            border: 0px;
+            margin-left: 500px
+        }
+
+        .button-file {
+            width: 102px;
+            /* margin-top: 5px; */
+            /* margin-left: 15.5em; */
+        }
+    </style>
 @endsection
 {{-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> --}}
 
